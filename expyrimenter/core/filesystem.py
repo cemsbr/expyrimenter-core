ab2116c05fa76a00a7ada7e43a23855be312f553
Carlos Eduardo Moreira dos Santos <cems@cemshost.com.br>
2015-04-19 19:47:16 +0900

    Filesystem helper

---
+++ expyrimenter/core/filesystem.py
@@ -0,0 +1,20 @@
+from . import SSH
+
+
+class Filesystem:
+    def __init__(self, path, host, logger_name=None):
+        self._path = path
+        self._host = host
+        self._logger_name = logger_name
+
+    def mkdir(self, title=None):
+        if title is None:
+            title = 'mkdir {} in {}'.format(self._path, self._host)
+
+        cmd = self._get_cmd()
+        return SSH(self._host, cmd, title=title, logger_name=self._logger_name)
+
+    def _get_cmd(self):
+        # Must not use quotes in dir name - no ~/ expansion
+        # TODO quoting
+        return 'test -d {} || mkdir -p {}'.format(self._path, self._path)
